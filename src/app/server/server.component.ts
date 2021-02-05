ce1cece9da19e4d2c3b828d6db492a3a7054576a
Muhammad <rydwan.dev@gmail.com>
2021-02-05 21:47:20 +0700

    Class binding with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,5 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'offline' ? 'red' : 'lightgreen';
+  }
+}
