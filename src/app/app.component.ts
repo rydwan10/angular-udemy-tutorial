234af18a113562084268a949ebfa6915e84488c2
Muhammad <rydwan.dev@gmail.com>
2021-02-09 20:54:18 +0700

    accessing ngForm using reference and viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  @ViewChild('f') signUpForm: NgForm;
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
 }
