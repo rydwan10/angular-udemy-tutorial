45f8ab953658da76f4e1fa680aeece5c976d247c
Muhammad <rydwan.dev@gmail.com>
2021-02-09 16:29:35 +0700

    replacing eventemitter with subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'myapp';
+  userActivated: boolean = false;
+  private activatedSub: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.activatedSub = this.userService.activatedEmitter.subscribe((value) => {
+      this.userActivated = value;
+    });
+  }
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Observable, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -28,18 +29,27 @@ export class HomeComponent implements OnInit, OnDestroy {
         count++;
       }, 1000);
     });
-    this.fristObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log('Completed');
-      }
-    );
+    this.fristObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data: number) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return 'Round ' + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('Completed');
+        }
+      );
   }
   ngOnDestroy(): void {
     this.fristObsSubscription.unsubscribe();
